ccba216d48874eac70ea2b061b96e04b3bfc98a9
Chandana Verriboyina <chandanaverriboyina@gmail.com>
2022-04-29 01:30:27 +0530

    initial commit

---
+++ src/com/app/App.java
@@ -0,0 +1,32 @@
+package com.app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.simplilearn.workshop.util.StringUtil;
+
+public class App {
+	public static Connection con=null;
+	public static Statement st=null;
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		
+				//Class.forName("com.mysql.cj.jdbc.Driver");
+				//con=DriverManager.getConnection("jdbc:mysql://localhost:3306/flyaway","root","Simplilearn");
+				//System.out.println("connection established with database");
+				//st=con.createStatement();
+				//ResultSet rs = st.executeQuery("select * from flyaway.flights");
+				//System.out.println(rs.getString("name"));
+						
+
+	}
+
+}
